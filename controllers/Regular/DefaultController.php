b252e29a9ef18ceedc0ecfaef92c171baf0edc35
Dmitry Gorokgovik <starijdmitry@icloud.com>
2022-08-01 16:57:33 +0300

    fix get news method

--- controllers/Regular/DefaultController.php
+++ controllers/Regular/DefaultController.php
@@ -34,9 +34,9 @@ class DefaultController extends Controller
             $data = $db->getAll("SELECT * FROM news WHERE id = '".$news_id."'");
         } else {
             if($step !== '0'){
-                $data = $db->getAll("SELECT * FROM news ORDER BY news_date DESC LIMIT '".$step."'");
-            } else {
                 $data = $db->getAll("SELECT * FROM news where id between '".$step."' and '".((float)$step+20)."'ORDER BY news_date DESC");
+            } else {
+                $data = $db->getAll("SELECT * FROM news ORDER BY news_date DESC LIMIT '".$step."'");
             }
         }
         return json_encode($data);
